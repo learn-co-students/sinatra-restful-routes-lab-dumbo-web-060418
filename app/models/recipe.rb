b631b44afdc268dc7dae9953ce6b23d55b3f8c21
QMaximillian <quinnlashinsky@gmail.com>
2018-06-27 22:39:04 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,23 @@
-#Placeholder for a model
+class Recipe < ActiveRecord::Base
+
+	# attr_accessor :name, :ingredients, :cook_time
+
+	# @@recipes = []
+
+	# def initialize(params)
+	# 	@name = params[:name]
+	# 	@ingredients = params[:ingredients]
+	# 	@cook_time = params[:cook_time]
+
+	# 	@@recipes << self
+	# end
+
+	# def self.all
+	# 	@@recipes
+	# end
+
+
+	# Recipe.new with name, ingredients, cooktime
+	# all returns array -- from the db
+
+end
